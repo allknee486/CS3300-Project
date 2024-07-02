a6b6608ec034243f7737699d9c7485354ac90818
Jacob <jnichol6@uccs.edu>
2024-07-02 14:44:35 -0600

    Bot control commands

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #bot.py
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,34 +9,43 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('Test')
 CHANNEL_ID= int(os.getenv('DISCORD_GENERAL'))
 VOICE_CHANNEL_ID = int(os.getenv('DISCORD_VOICE_GENERAL'))
-intents = discord.Intents.default()
+
+intents = discord.Intents.all()
 intents.members=True
 
-client = discord.Client(intents=intents)
-
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents)
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if guild.name == GUILD:
             break
 
     print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name} (id: {guild.id})\n'
     )
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-    #channel = client.get_channel(CHANNEL_ID)
-    #if channel:
-    #   await channel.send('Hello, this is a message from the bot!')
-
-    voice_channel = client.get_channel(VOICE_CHANNEL_ID)
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    voice_channel = bot.get_channel(VOICE_CHANNEL_ID)
     if isinstance(voice_channel, discord.VoiceChannel):
         await voice_channel.connect()
 
-client.run(TOKEN)
+@bot.command(name='leave', help='Tells the bot to leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.guild.voice_client
+    if voice_client and voice_client.is_connected():
+        await voice_client.disconnect()
 
+@bot.command(name='message', help='Tells the bot to send a message to the specified channel')
+async def message(ctx):
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send('Hello, this is a message from the bot!')
+
+bot.run(TOKEN)
