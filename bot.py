ebe7fd6b2a5bd6d51202d021cb0338474a17edad
AmanAK03 <aman7907@gmail.com>
2024-07-15 17:32:26 -0600

    Shortened command names, added translator and TTS.

--- bot.py
+++ bot.py
@@ -4,6 +4,10 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import shutil
 import whisper
+from deep_translator import GoogleTranslator
+from gtts import gTTS
+
+language_dic = {'afrikaans': 'af', 'albanian': 'sq', 'amharic': 'am', 'arabic': 'ar', 'armenian': 'hy', 'assamese': 'as', 'aymara': 'ay', 'azerbaijani': 'az', 'bambara': 'bm', 'basque': 'eu', 'belarusian': 'be', 'bengali': 'bn', 'bhojpuri': 'bho', 'bosnian': 'bs', 'bulgarian': 'bg', 'catalan': 'ca', 'cebuano': 'ceb', 'chichewa': 'ny', 'chinese (simplified)': 'zh-CN', 'chinese (traditional)': 'zh-TW', 'corsican': 'co', 'croatian': 'hr', 'czech': 'cs', 'danish': 'da', 'dhivehi': 'dv', 'dogri': 'doi', 'dutch': 'nl', 'english': 'en', 'esperanto': 'eo', 'estonian': 'et', 'ewe': 'ee', 'filipino': 'tl', 'finnish': 'fi', 'french': 'fr', 'frisian': 'fy', 'galician': 'gl', 'georgian': 'ka', 'german': 'de', 'greek': 'el', 'guarani': 'gn', 'gujarati': 'gu', 'haitian creole': 'ht', 'hausa': 'ha', 'hawaiian': 'haw', 'hebrew': 'iw', 'hindi': 'hi', 'hmong': 'hmn', 'hungarian': 'hu', 'icelandic': 'is', 'igbo': 'ig', 'ilocano': 'ilo', 'indonesian': 'id', 'irish': 'ga', 'italian': 'it', 'japanese': 'ja', 'javanese': 'jw', 'kannada': 'kn', 'kazakh': 'kk', 'khmer': 'km', 'kinyarwanda': 'rw', 'konkani': 'gom', 'korean': 'ko', 'krio': 'kri', 'kurdish (kurmanji)': 'ku', 'kurdish (sorani)': 'ckb', 'kyrgyz': 'ky', 'lao': 'lo', 'latin': 'la', 'latvian': 'lv', 'lingala': 'ln', 'lithuanian': 'lt', 'luganda': 'lg', 'luxembourgish': 'lb', 'macedonian': 'mk', 'maithili': 'mai', 'malagasy': 'mg', 'malay': 'ms', 'malayalam': 'ml', 'maltese': 'mt', 'maori': 'mi', 'marathi': 'mr', 'meiteilon (manipuri)': 'mni-Mtei', 'mizo': 'lus', 'mongolian': 'mn', 'myanmar': 'my', 'nepali': 'ne', 'norwegian': 'no', 'odia (oriya)': 'or', 'oromo': 'om', 'pashto': 'ps', 'persian': 'fa', 'polish': 'pl', 'portuguese': 'pt', 'punjabi': 'pa', 'quechua': 'qu', 'romanian': 'ro', 'russian': 'ru', 'samoan': 'sm', 'sanskrit': 'sa', 'scots gaelic': 'gd', 'sepedi': 'nso', 'serbian': 'sr', 'sesotho': 'st', 'shona': 'sn', 'sindhi': 'sd', 'sinhala': 'si', 'slovak': 'sk', 'slovenian': 'sl', 'somali': 'so', 'spanish': 'es', 'sundanese': 'su', 'swahili': 'sw', 'swedish': 'sv', 'tajik': 'tg', 'tamil': 'ta', 'tatar': 'tt', 'telugu': 'te', 'thai': 'th', 'tigrinya': 'ti', 'tsonga': 'ts', 'turkish': 'tr', 'turkmen': 'tk', 'twi': 'ak', 'ukrainian': 'uk', 'urdu': 'ur', 'uyghur': 'ug', 'uzbek': 'uz', 'vietnamese': 'vi', 'welsh': 'cy', 'xhosa': 'xh', 'yiddish': 'yi', 'yoruba': 'yo', 'zulu': 'zu'}
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -65,25 +69,37 @@ async def finished_callback(sink, ctx):
     else:
         await ctx.send("No audio was recorded or no valid audio file found.")
 
-@bot.command(name='translate_audio', help='Transcribes the audio in the specified language')
+@bot.command(name='translate', help='Translates the audio transcription into the specified language')
 async def translate_audio(ctx, mention: str, language: str):
     user_id = int(mention.strip('<@!>'))
     audio_path = next((path for path in bot.temp_audio_files if str(user_id) in path), None)
     if audio_path:
         try:
             if os.path.getsize(audio_path) > 0:
-                result = model.transcribe(audio_path, language=language)
-                transcript = result.get('text', 'Transcription not available')
-                await ctx.send(f"Transcription for <@{user_id}>: {transcript}")
+                result = model.transcribe(audio_path)
+                transcription = result.get('text', 'Transcription not available')
+                if transcription != 'Transcription not available':
+                    translated_text = GoogleTranslator(source='auto', target=language).translate(transcription)
+                    await ctx.send(f"Transcription for <@{user_id}>: {translated_text}")
+
+                    tts = gTTS(text=translated_text, lang=language_dic.get(language))
+                    tts_path = os.path.join(os.getcwd(), "tts_output.mp3")
+                    tts.save(tts_path)
+
+                    if ctx.voice_client and ctx.voice_client.is_connected():
+                        ctx.voice_client.play(discord.FFmpegPCMAudio(tts_path))
+                    else:
+                        await ctx.send("Bot is not connected to a voice channel.")
+                else:
+                    await ctx.send("Transcription not available.")
             else:
                 await ctx.send("Audio file is empty. No transcription made.")
         except Exception as e:
             await ctx.send(f"An error occurred: {str(e)}")
     else:
         await ctx.send("Audio file not found.")
-
-@bot.command(name='stop_recording', help='Stops the recording')
-async def stop_recording(ctx):
+@bot.command(name='stop', help='Stops the recording')
+async def stop(ctx):
     voice_client = ctx.guild.voice_client
     if ctx.voice_client:
         ctx.voice_client.stop_recording()
